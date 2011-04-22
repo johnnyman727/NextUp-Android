9898d4ffef8ba7ffd9cda808875dbd1c95342ff3
Jon McKay <jonathan.mckay@students.olin.edu>
2011-04-22 00:43:32 -0400

    Added the LocationManager

---
+++ src/com/dotcom/nextup/classes/FoursquareLocationManager.java
@@ -0,0 +1,109 @@
+package com.dotcom.nextup.classes;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.dotcom.nextup.categorymodels.CheckIn;
+import com.dotcom.nextup.categorymodels.CheckInManager;
+import com.google.android.maps.GeoPoint;
+
+public class FoursquareLocationManager {
+	
+	public static GeoPoint getLastLocation(ArrayList<CheckIn> checkIns, CheckInManager checkInManager) {
+    	if (checkIns != null) {
+    		CheckIn lastCheckIn = checkInManager.getLastCheckIn(checkIns);
+    		return lastCheckIn.getLocation();
+    	}
+    	return null;
+    }
+    
+    public static String getLastLocationName(ArrayList<CheckIn> checkIns, CheckInManager checkInManager) {
+    	if (checkIns != null) {
+    		CheckIn lastCheckIn = checkInManager.getLastCheckIn(checkIns);
+    		return lastCheckIn.getName();
+    	}
+    	return null;
+    }
+    
+    public static JSONArray getCurrentLocationDataFromFoursquare(GeoPoint location, String token) {
+		if (token == null) {
+			return null;
+		}
+		String url = "https://api.foursquare.com/v2/venues/search?ll=" + String.valueOf((location.getLatitudeE6()/1E6)) + "," + String.valueOf((location.getLongitudeE6()/1E6));
+		String authUrl = url + "&oauth_token=" + token;
+		HttpClient hc = new DefaultHttpClient();
+	
+		try {
+			HttpGet request = new HttpGet(authUrl);
+			HttpResponse resp = hc.execute(request);
+			HttpEntity entity = resp.getEntity();
+			String contentString = CheckInManager.convertStreamToString(entity.getContent());
+			JSONObject responseObj = new JSONObject(contentString);
+			int responseCode = resp.getStatusLine().getStatusCode();
+
+			if (responseCode >= 200 && responseCode < 300) {
+				JSONObject res = responseObj.getJSONObject("response");
+				JSONArray groups = res.getJSONArray("groups");
+				JSONObject nearby = groups.getJSONObject(1);
+				JSONArray close = nearby.getJSONArray("items");
+				return close;
+			} 
+		}catch (IllegalStateException e) {
+			//TODO: Deal with this error
+			e.printStackTrace();
+		} catch (IOException e) {
+			//TODO: Deal with this error
+			e.printStackTrace();
+		} catch (JSONException e) {
+			//TODO: Deal with this error
+			e.printStackTrace();
+		}
+		return null;
+	}
+    
+    public static Venue getNearestLocationFromFoursquare(ArrayList<Venue> venues) {
+    	if (venues == null) 
+    		return null;
+    	return venues.get(0);
+    }
+    
+	public static ArrayList<Venue> getNearbyLocationsFromFoursquare(JSONArray close, ArrayList<Venue> nearby_locations) throws JSONException {
+		if (nearby_locations == null || close == null)
+			return null;
+		nearby_locations.clear();
+		for (int i = 0; i < close.length(); i++) {
+			JSONObject nearbyPlace = close.getJSONObject(i);
+			JSONObject location = nearbyPlace.getJSONObject("location");
+			int lat = (int) (Double.parseDouble(location.getString("lat")) * 1E6);
+			int lon = (int) (Double.parseDouble(location.getString("lng")) * 1E6);
+			GeoPoint locationGeoPoint = new GeoPoint(lat, lon);
+			Venue newVenue;
+			try {
+				newVenue = new Venue(nearbyPlace.getString("name"), "url", "image url", locationGeoPoint, Integer.parseInt(location.getString("distance")));
+				nearby_locations.add(newVenue);
+			} catch (NumberFormatException e) {
+				//TODO: Deal with this error
+				e.printStackTrace();
+			} catch (JSONException e) {
+				//TODO: Deal with this error
+				e.printStackTrace();
+			}
+		}
+		return nearby_locations;
+	}
+    
+	public boolean sameLocation(GeoPoint l1, GeoPoint l2) {
+		if ((l1.getLongitudeE6() == l2.getLongitudeE6()) && (l1.getLatitudeE6() == l2.getLatitudeE6()))
+			return true;
+		return false;
+	}
+}
