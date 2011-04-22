ea066467a8a02db4ba6bca2f48d58dbc0bd8875b
Noura <noura.howell@students.olin.edu>
2011-04-21 22:05:18 -0400

    recommendation engine, if given more than 3 categories, will now choose the three most appropriate categories and search only for those.

--- src/com/dotcom/nextup/yelp/Yelp.java
+++ src/com/dotcom/nextup/yelp/Yelp.java
@@ -3,7 +3,9 @@ package com.dotcom.nextup.yelp;
 Example code based on code from Nicholas Smith at http://imnes.blogspot.com/2011/01/how-to-use-yelp-v2-from-java-including.html
 For a more complete example (how to integrate with GSON, etc) see the blog post above.
 */
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 
 import org.json.JSONArray;
@@ -48,9 +50,43 @@ public class Yelp {
 	
 	public ArrayList<YelpVenue> getRecommendation(RecommendationInput input) {
 		/* THE RECOMMENDATION ENGINE */
-		ArrayList<YelpVenue> all_venues = getManyPossibleVenues(input);
-		ArrayList<YelpVenue> rec = chooseBest(input, all_venues);
+		RecommendationInput input2 = narrowDownCategories(input);
+		ArrayList<YelpVenue> all_venues = getManyPossibleVenues(input2);
+		ArrayList<YelpVenue> rec = chooseBest(input2, all_venues);
 		return rec;
+	}
+	
+	private RecommendationInput narrowDownCategories(RecommendationInput in) {
+		
+		int now = getCurrentHours();
+		ArrayList<Category> all_cats = in.getCategories();
+		ArrayList<Category> best_cats = new ArrayList<Category>();
+		ArrayList<NumObjectPair> options = new ArrayList<NumObjectPair>(); // will assign rank to each venue, then select venues with highest ranks
+		for ( int i = 0; i < all_cats.size(); i++ ) {
+			Category cat = all_cats.get(i);
+			options.add(new NumObjectPair(rankCategory(cat, now), cat));
+		}
+		Collections.sort(options); // sorts lowest to highest by num
+		
+		int i = 0;
+		while ( best_cats.size() < 3 && i < all_cats.size()) {
+			Category cat = (Category) options.get(i).getObject();
+			if ( ! isIn(cat, best_cats) ) best_cats.add(cat);
+			i++;
+		}
+
+		RecommendationInput out = new RecommendationInput(best_cats, in.getLatitude(), in.getLongitude(), in.getMaxDistance());
+		return out;
+	}
+	
+	private int rankCategory(Category cat, int now) {
+		return abs(cat.getAverageTime()-now);
+	}
+	
+	public static Integer getCurrentHours() {
+		Calendar cal = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("HH");
+		return Integer.parseInt(sdf.format(cal.getTime()));
 	}
 	
 	public ArrayList<YelpVenue> getManyPossibleVenues(RecommendationInput input) {
@@ -129,10 +165,10 @@ public class Yelp {
 		 * sort: list of (rank, venue) pairs by their rank
 		 * undecorate: from list of venues sorted by rank, choose the highest-ranked venue
 		 */
-		ArrayList<RankVenuePair> options = new ArrayList<RankVenuePair>(); // will assign rank to each venue, then select venues with highest ranks
+		ArrayList<NumObjectPair> options = new ArrayList<NumObjectPair>(); // will assign rank to each venue, then select venues with highest ranks
 		for ( int i = 0; i < all_venues.size(); i++ ) {
 			venue = all_venues.get(i);
-			options.add(new RankVenuePair(rank(venue), venue));
+			options.add(new NumObjectPair(rankYelpVenue(venue), venue));
 		}
 		Collections.sort(options); // I think the ones at the end have higher ranks
 		/*for ( int i = options.size() - 1; i >= options.size() - 3; i--) {
@@ -141,7 +177,7 @@ public class Yelp {
 		
 		int i = options.size() -1;
 		while ( best.size() < 3 && i >= 0) {
-			YelpVenue ven = options.get(i).getVenue();
+			YelpVenue ven = (YelpVenue) options.get(i).getObject();
 			if ( ! isIn(ven, best) ) best.add(ven);
 			i--;
 		}
@@ -149,48 +185,66 @@ public class Yelp {
 	}
 
 	private boolean isIn(YelpVenue venue, ArrayList<YelpVenue> venues) {
-		boolean res = false;
 		for (YelpVenue ven : venues) {
-			if ( venue.hasSameNameAs(ven)) res = true;
+			if ( venue.hasSameNameAs(ven)) return true;
 		}
-		return res;
+		return false;
 	}
 	
-	private double rank(YelpVenue venue) {
+	private boolean isIn(Category kitty, ArrayList<Category> cats) {
+		for (Category cat : cats) {
+			if ( kitty.getName().equals(cat.getName())) return true;
+		}
+		return false;
+	}
+	
+	private double rankYelpVenue(YelpVenue venue) {
 		/* if a venue has too few reviews, its rating doesn't mean much */
 		if ( venue.getReviewCount() > 3 ) { return venue.getRating(); }
 		else { return venue.getRating()/2; }
 	}
 	
-	public class RankVenuePair implements Comparable<RankVenuePair> {
-		/* for comparing venues by their rank */
-		private double rank;
-		private YelpVenue venue;
+	public class NumObjectPair implements Comparable<NumObjectPair> {
+		/* for when you want to sort a list of objects based on 
+		 * a number assigned to each object
+		 * 
+		 * convert the list of objects into a list of NumObjectPair
+		 * and use Collections.sort, then extract the objects back out
+		 */
+		private double n;
+		private Object o;
 		
-		public RankVenuePair(double rank, YelpVenue venue) {
-			this.rank = rank;
-			this.venue = venue;
+		public NumObjectPair(double n, Object o) {
+			this.n = n;
+			this.o = o;
 		}
-
-		public double getRank() { return rank; }
-		public YelpVenue getVenue() { return venue; }
 		
-		public int equals(RankVenuePair another) {
-			return Double.compare((Double) this.getRank(), (Double) another.getRank());
+		public double getNum() { return n; }
+		public Object getObject() { return o; }
+		
+		public int equals(NumObjectPair another) {
+			// I think this method is actually super sketch and hopefully never gets called
+			// but I'm gonna leave it be
+			return Double.compare((Double) this.getNum(), (Double) another.getNum());
 		}
-
+		
 		@Override
-		public int compareTo(RankVenuePair another) {
-			return Double.compare((Double) this.getRank(), (Double) another.getRank());
+		public int compareTo(NumObjectPair another) {
+			return Double.compare((Double) this.getNum(), (Double) another.getNum());
 		}
 		
 		public String toString() {
-			return "RankVenuePair rank " + Double.toString(rank) + " venue " + venue.toString();
+			return "NumObjectPair num " + Double.toString(n) + " venue " + o.toString();
 		}
 	}
 	
 	public int min (int a, int b) {
 		if (a < b) { return a; }
 		else { return b; }
+	}
+	
+	public int abs (int a) {
+		if (a < 0) { return -1*a; }
+		else { return a; }
 	}
 }
