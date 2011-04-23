94f09dba9c1ef2e4275822e0c0ec9cf07b2fdd5d
Jon McKay <jonathan.mckay@students.olin.edu>
2011-04-23 02:29:23 -0400

    A few more attempts at finding random null pointer errors

---
+++ src/com/dotcom/nextup/classes/RecommendationEngine.java
@@ -0,0 +1,27 @@
+package com.dotcom.nextup.classes;
+
+import java.util.ArrayList;
+
+import com.dotcom.nextup.categorymodels.Category;
+
+public class RecommendationEngine {
+	ArrayList<Category> categories;
+	double latitude;
+	double longitude;
+	double max_distance; // in meters, cut off distance for how far away a venue can be, should vary by transportation type
+	
+	public RecommendationEngine(ArrayList<Category> categories, double lat, double lon, double distance) {
+		this.categories = categories;
+		this.latitude = lat;
+		this.longitude = lon;
+		this.max_distance = distance;
+	}
+	
+	public ArrayList<Category> getCategories() {return this.categories;}
+	public void setCategories(ArrayList<Category> cats) {this.categories = cats;}
+	public double getLatitude() {return this.latitude;}
+	public void setLatitude(double lat) {this.latitude = lat;}
+	public double getLongitude() {return this.longitude;}
+	public void setLongitude(double lon) {this.latitude = lon;}
+	public double getMaxDistance() { return this.max_distance; }
+}
